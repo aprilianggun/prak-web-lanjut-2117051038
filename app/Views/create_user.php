24858d9493ee883caba279635324a69904204d8e
aprilianggun <aprilia.anggun21@students.unila.ac.id>
2023-10-05 23:11:35 +0700

    database

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -4,28 +4,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url("assets/css/profile.css") ?>">
+    <link rel="stylesheet" href="<?= base_url("assets/css/form.css") ?>">
     <title>Document</title>
 </head>
 
 <body>
     <div class="card">
-        <div class="form-container">
-            <form action="<?= base_url('user/store') ?>" method="post">
-                <div class="form-input">
-                    <input type="text" name="nama" placeholder="Nama" required />
-                </div>
-                <div class="form-input">
-                    <input type="text" name="npm" placeholder="NPM" required />
-                </div>
-                <div class="form-input">
-                    <input type="text" name="kelas" placeholder="Kelas" required />
-                </div>
-                <input type="submit" value="Submit" class="btn-login" />
-            </form>
+        <div class="card">
+            <div class="form-container">
+                <form action="<?= base_url('user/store') ?>" method="post">
+                    <div class="form-input">
+                        <input type="text" name="nama" placeholder="Nama" required />
+                    </div>
+                    <div class="form-input">
+                        <input type="text" name="npm" placeholder="NPM" required />
+                    </div>
+                    <tr>
+                        <td>Kelas</td>
+                        <td>:</td>
+                        <td>
+                            <select name="id_kelas" id="kelas">
+                                <?php if (isset($kelas) && is_array($kelas)) : ?>
+                                    <?php foreach ($kelas as $item) : ?>
+                                        <option value="<?= $item['id'] ?>"><?= $item['nama_kelas'] ?></option>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <input type="submit" value="Submit" class="btn-login" />
+                </form>
+            </div>
         </div>
-    </div>
 
+    </div>
 </body>
 
 </html>
--- app/Views/profile.php
+++ app/Views/profile.php
@@ -12,7 +12,7 @@
     <div class="card">
     <img src="<?= base_url('/assets/img/aprikcantik.jpeg') ?>" alt="cantik" width=130 height=130 class="gambar"> <br>
         Nama: <?= $nama ?><br>
-        Kelas: <?= $kelas ?><br>
+        Kelas: <?= $id_kelas ?><br>
         NPM: <?= $npm ?>
     </div>
 </body>
