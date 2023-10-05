3319190531a814b2d78dfef37993af5ac25705a7
aprilianggun <aprilia.anggun21@students.unila.ac.id>
2023-10-05 22:10:42 +0700

    form

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'UserController::profile/$1/$2/$3');
+
+#create
+$routes->get('/user/create', 'UserController::create');
+$routes->post('/user/store', 'UserController::store');
 
