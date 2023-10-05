ff377e53fc0fec56d43bfc47317c491219e600d8
aprilianggun <aprilia.anggun21@students.unila.ac.id>
2023-10-05 14:53:35 +0700

    biodata

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url("assets/css/profile.css") ?>">
     <title>Document</title>
 </head>
+
 <body>
-    Nama:  <?= $nama ?><br>
-    Kelas: <?= $kelas ?><br>
-    NPM: <?= $npm ?>
+    <div class="card">
+    <img src="<?= base_url('/assets/img/aprikcantik.jpeg') ?>" alt="cantik" width=130 height=130 class="gambar"> <br>
+        Nama: <?= $nama ?><br>
+        Kelas: <?= $kelas ?><br>
+        NPM: <?= $npm ?>
+    </div>
 </body>
+
 </html>
